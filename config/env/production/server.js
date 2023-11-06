c9553cffa864eea55bb273cc87353df204a0ae11
carlos lopez <cloaction@gmail.com>
2023-11-06 20:44:14 +0100

    render

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,7 @@
 module.exports = ({ env }) => ({
-  url: env("RENDER_EXTERNAL_URL"),
+  proxy: true,
+  url: env('HOST_URL'), // Sets the public URL of the application.
+  app: {
+    keys: env.array('APP_KEYS')
+  },
 });
